1e28406f90a458a502630da077ee0a8e3a36e94b
li lin <lovelyappleoffie@gmial.com>
2025-10-29 19:13:42 +0900

    定数をちょっと整理

--- TargetShoot/Assets/Scripts/Sytem/GameDefinitions.cs
+++ TargetShoot/Assets/Scripts/Sytem/GameDefinitions.cs
@@ -14,19 +14,24 @@ namespace GameDefinition
     public static class GameConstant
     {
         public const int ApplicationVersion = 1;
+
+        // 物理関連
         public const float MoveLimit = 3f;
         public const float MoveSpeed = 2f;
         public const float TargetPlateDistance = 0.5f;
+        public const float TargetMoveBaseSpeed = 0.2f;
+        public const float BulletSpeed = 5f;
+        public const float TargetDispearLimitHeight = -2f;
+
+        // ゲームバランス
         public const int HighScoreTargetProbability = 10;
         public const int DownTargetProbability = 20;
         public const int MaxProbability = 100;
-        public const float TargetMoveBaseSpeed = 0.2f;
-        public const float BulletSpeed = 5f;
-        public const int DefaultScore = 50;
 
-        public const float TargetDispearLimitHeight = -2f;
-        public const float TargetReviveInterval = 2f;
-        public const int ComboBonusTimes = 2;
+        // スコアー関連
         public const int FireCost = 1;
+        public const int DefaultScore = 50;
+        public const float TargetReviveInterval = 2f;
+        public const int BulletComboBonusScoreTimes = 2;
     }
 }
