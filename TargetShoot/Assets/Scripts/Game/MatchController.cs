c4e0f35115aca2fe7c8f33d35309f703b0182ce7
li lin <lovelyappleoffie@gmial.com>
2025-10-29 18:38:00 +0900

    fix

--- TargetShoot/Assets/Scripts/Game/MatchController.cs
+++ TargetShoot/Assets/Scripts/Game/MatchController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class MatchController : MonoBehaviour
 {
     [SerializeField] FieldController FieldController;
-    public ScoreComboManager ScoreComboManager { getg; private set; }
+    public ScoreComboManager ScoreComboManager { get; private set; }
     private void Awake()
     {
         ScoreComboManager = new ScoreComboManager();
